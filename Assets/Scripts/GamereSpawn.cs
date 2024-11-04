88c33c524685fe7d235ee5b25083b1313b055647
Axeltaylor36 <alejandrosastre36@gmail.com>
2024-11-04 21:28:52 +0100

    Respawn solucionado

--- Assets/Scripts/GamereSpawn.cs
+++ Assets/Scripts/GamereSpawn.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Gamerespawn : MonoBehaviour
 {
     [SerializeField] private float treshold;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+
     }
 
     // Update is called once per frame
@@ -21,6 +24,9 @@ public class Gamerespawn : MonoBehaviour
         if (transform.position.y < treshold)
         {
             transform.position = new Vector3(6.08f, 133.9f, 33.7f);
+            rb.isKinematic = true;
+            rb.velocity = Vector3.zero;
+            rb.isKinematic = false;
         }
     }
 }
